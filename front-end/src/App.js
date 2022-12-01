ba8d9edc1c4c48e77cb467d43e7ceec9b4ff0988
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-01 12:43:44 +0000

    Add landing page to app file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <h1>This is our React Project!!!</h1>
+      <LandingPage />
     </div>
   );
 }
