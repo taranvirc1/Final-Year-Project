61806553d4e8e90e52eef86baaa00105a5e84658
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-03 23:38:33 +0000

    Update MapSection and App files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import MapSection from "./components/MapSection";
 
 function App() {
   return (
     <div className="App">
       <h1>This is our React Project!!!</h1>
       <LandingPage />
+      <MapSection />
     </div>
   );
 }
