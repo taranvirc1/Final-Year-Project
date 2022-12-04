10eb76ff9b3f62339033f944b6edf0939561dc72
Ajama02 <118631899+Ajama02@users.noreply.github.com>
2022-12-04 21:29:36 +0000

    Imported Footer
    Imported my footer from components folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <h1>This is our React Project!!!</h1>
       <LandingPage />
+      <Footer />
     </div>
   );
 }
