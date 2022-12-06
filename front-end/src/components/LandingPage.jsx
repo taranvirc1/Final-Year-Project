53e764ffc87ae4fd3478012650b08a6889927ef2
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-06 13:04:50 +0000

    Update LandingPage

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../components/ImageSlider";
 import ImageSlider from "../components/ImageSlider";
+import MapSection from "../components/MapSection";
+import Footer from "../components/Footer";
 
 import Navbar from "./Navbar";
 function LandingPage() {
@@ -8,6 +10,8 @@ function LandingPage() {
     <div>
       <Navbar />
       <ImageSlider />
+      <MapSection />
+      <Footer />
     </div>
   );
 }
