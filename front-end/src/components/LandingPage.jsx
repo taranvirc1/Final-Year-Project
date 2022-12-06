6748a1deb870ba46aa04a6e209a43987496ffb41
KENURA <pererakenura@gmail.com>
2022-12-06 12:12:09 +0000

    Update LandingPage.jsx

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import ImageSlider from "./ImageSlider";
 import Navbar from "./Navbar";
 function LandingPage() {
   return <div>This is the landing page!!!
        <Navbar/>
-       <ImageSlider />
     
   </div>;
 }
