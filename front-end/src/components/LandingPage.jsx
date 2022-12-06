dc05de34a8ea2adbebc94032b0124fd7fef9cba9
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-06 12:25:12 +0000

    Update LandingPage file

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../components/ImageSlider";
+import ImageSlider from "../components/ImageSlider";
 
 import Navbar from "./Navbar";
 function LandingPage() {
   return (
     <div>
       <Navbar />
+      <ImageSlider />
     </div>
   );
 }
