80eb4c3517ab09b43152c73c30dbca346cb2c192
Murad-gif <2051290@brunel.ac.uk>
2022-12-06 11:53:14 +0000

    Updated Nav Bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage";
-import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <h1>This is our React Project!!!</h1>
    
       <LandingPage />
-      <Footer />
+      
     </div>
   );
 }
