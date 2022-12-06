24bd36ac5270702b5acef05e67cf7de38ff81163
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-06 12:18:02 +0000

    Update LandingPage.jsx

--- front-end/src/components/LandingPage.jsx
+++ front-end/src/components/LandingPage.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 import Navbar from "./Navbar";
 function LandingPage() {
-  return <div>This is the landing page!!!
-       <Navbar/>
-<<<<<<< HEAD
-        
-=======
-       <ImageSlider />
-    
->>>>>>> 8c5baa49cde3a26aafae503c93f03f851b75babb
-  </div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default LandingPage;
