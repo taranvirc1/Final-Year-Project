cf2ec5283ad256ae9ae448eb42dc68ab1f17cf20
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-07 12:08:43 +0000

    Update WhoAreWe.jsx

--- front-end/src/components/WhoAreWe.jsx
+++ front-end/src/components/WhoAreWe.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import "../Styles/WhoAreWe.css"
+import React from "react";
+import "../Styles/WhoAreWe.css";
 
 export default function WhoAreWe() {
   return (
-    <div className='body'>
-    <div class="title">
+    <body style={{ background: "azure" }}>
+      <div class="title">
         <h1>Who Are We?</h1>
-    </div>
-     <div class="center">
+      </div>
+      <div class="center">
         <div class="box1">
-            <img src="Picture1.png" height="250px" width="400px"/>
+          <img src="Picture1.png" height="250px" width="400px" alt="picture1" />
         </div>
         <div class="box2">
-            <p>Coding4All was developed by a team of young developers, at Brunel Univeristy, with the sole purpose to change the lives of other students.</p>
+          <p>
+            Coding4All was developed by a team of young developers, at Brunel
+            Univeristy, with the sole purpose to change the lives of other
+            students.
+          </p>
         </div>
         <div class="box3">
-            <p>Our team have a strong passion for coding and learning. They wish to provide useful resources so that other people may be able to nuture the same passion as they have. </p>
+          <p>
+            Our team have a strong passion for coding and learning. They wish to
+            provide useful resources so that other people may be able to nuture
+            the same passion as they have.{" "}
+          </p>
         </div>
-        <div class = "box4">
-            <img src="Picture1.png" height="250px" width="400px"/>
+        <div class="box4">
+          <img src="Picture1.png" height="250px" width="400px" alt="picture2" />
         </div>
-     </div>  
-    </div>
-  )
+      </div>
+    </body>
+  );
 }
