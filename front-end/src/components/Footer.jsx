decc9c05e3aeacd40d0809759095256fcfbf3389
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-14 11:42:54 +0000

    Update Footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -9,96 +9,8 @@ import "../Styles/Footer.css";
 
 function Footer() {
   return (
-    // <div className="footer">
-    //   <div className="footer-container-left">
-    //     {/* <div className="container-items"> */}
-    //     {/* Section_1 */}
-    //     <div className="about">
-    //       <img
-    //         className="footer-logo"
-    //         src={logo}
-    //         alt="logo"
-    //         style={{ width: 140 }}
-    //       ></img>
-    //       <h3 className="footer-about-us">About Us</h3>
-    //       <p>
-    //         Coding4All was developed by a team of young developers, at Brunel
-    //         University, with the sole purpose to change the lives of other
-    //         students.
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <ul className="footer-container-right">
-    //     {/* Section_2 */}
-    //     {/* <div className="contact-us"> */}
-    //     {/* <ul className="link"> */}
-    //     <li>
-    //       <h3 className="footer-title">Contact Us</h3>
-    //       {/* </li> */}
-    //       <ul className="footer-box">
-    //         <li>
-    //             <a href="/">Code4All@hotmail.com</a>
-    //         </li>
-    //         <li>
-    //           <p>Phone: +44 123-456-7890</p>
-    //         </li>
-    //       </ul>
-    //     </li>
-    //     {/* </ul> */}
-    //     {/* </div> */}
-    //     {/* Section_3 */}
-    //     {/* <div className="links"> */}
-    //     {/* <ul className="link"> */}
-    //     <li>
-    //       <h3 className="footer-title">Links</h3>
-    //       {/* </li> */}
-    //       <ul className="footer-box">
-    //         <li>
-    //           <a href="/donate">Donate</a>
-    //         </li>
-    //         <li>
-    //           <a href="/faq">F.A.Q</a>
-    //         </li>
-    //         <li>
-    //           <a href="/courses">Courses</a>
-    //         </li>
-    //       </ul>
-    //     </li>
-    //     {/* </ul> */}
-    //     {/* </div> */}
-    //     {/* Section_4*/}
-    //     {/* <div className="socials"> */}
-    //     {/* <ul> */}
-    //     <li>
-    //       <h3 className="footer-title">Socials</h3>
-    //       <ul className="footer-box">
-    //         <li>
-    //           <a href="http://www.facebook.com">
-    //             <AiFillFacebook />
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="http://www.instagram.com">
-    //             <AiFillInstagram />
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="http://www.Twitter.com">
-    //             <AiFillTwitterCircle />
-    //           </a>
-    //         </li>
-    //       </ul>
-    //       {/* </ul> */}
-    //       {/* </div> */}
-    //     </li>
-    //   </ul>
-    //   <div className="footer-container-bottom">
-    //     <p>All Rights Reserved by &copy;Code4All 2022</p>
-    //   </div>
-    //   {/* </div> */}
-    // </div>
-
     <div className="footer">
+      {/* Section 1 */}
       <div className="footer-container-left">
         <img src={logo} alt="logo" style={{ width: 140 }} />
         <h3 className="footer-about-us">About Us</h3>
@@ -108,7 +20,7 @@ function Footer() {
           Sunt repudiandae architecto voluptas praesentium possimus aliquid.
         </p>
       </div>
-
+      {/* Section 2 */}
       <ul className="footer-container-right">
         <li>
           <h3 className="footer-title">Contact Us</h3>
@@ -125,7 +37,7 @@ function Footer() {
             </li>
           </ul>
         </li>
-
+        {/* Section 3 */}
         <li className="features">
           <h3 className="footer-title">Links</h3>
           <ul className="footer-box link h-box">
