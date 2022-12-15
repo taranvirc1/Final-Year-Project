4dd1383f2e58f270f66148f00b1c75a0b7c0e5a1
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2022-12-15 18:15:11 +0000

    Create ConfirmAccount page

--- front-end/src/AppRoute.js
+++ front-end/src/AppRoute.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import LandingPage from "./components/LandingPage";
 import Account from "./components/RegisterLogin/Account";
+import ConfirmAccount from "./components/RegisterLogin/ConfirmAccount";
 import NewPassword from "./components/RegisterLogin/NewPassword";
 import ResetPassword from "./components/RegisterLogin/ResetPassword";
 
@@ -11,12 +12,13 @@ function AppRoute() {
     <div>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />}>
-                <Route index element={<LandingPage />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/resetPassword" element={<ResetPassword />}/>
-                <Route path="/newPassword" element={<NewPassword />}/>         
-            </Route>
+          <Route path="/" element={<App />}>
+            <Route index element={<LandingPage />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/resetPassword" element={<ResetPassword />} />
+            <Route path="/newPassword" element={<NewPassword />} />
+            <Route path="/confirmAccount" element={<ConfirmAccount />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
