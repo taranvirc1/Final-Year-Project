465c2ffd8c3471ff4c77c7bada3b2fa77865290e
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-01-30 09:30:03 +0000

    Create StudentPostDTO class in back end

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/dto/StudentPostDTO.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/dto/StudentPostDTO.java
@@ -1,5 +1,83 @@
 package CS2001.Group47.ELearning_Platform.dto;
 
+import java.util.Date;
+
 public class StudentPostDTO {
 	
+    String firstName;
+    String lastName;
+    Date dateoOfBirth;
+    String country;
+    String phone;
+    String email;
+    String password;
+    
+    public StudentPostDTO(String firstName, String lastName, Date dateoOfBirth, String country, String phone,
+            String email, String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateoOfBirth = dateoOfBirth;
+        this.country = country;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateoOfBirth() {
+        return dateoOfBirth;
+    }
+
+    public void setDateoOfBirth(Date dateoOfBirth) {
+        this.dateoOfBirth = dateoOfBirth;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
