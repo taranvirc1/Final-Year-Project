54deb3fc51ed35f2abbe0ffef8f6c0f4e650a40c
Murad-gif <2051290@brunel.ac.uk>
2023-02-04 01:00:10 +0000

    creating model element for videos section

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Videos.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Videos.java
@@ -1,4 +1,5 @@
 package CS2001.Group47.ELearning_Platform.model;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,9 +11,39 @@ import javax.persistence.Table;
 public class Videos {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(table="videoID")
 	private Integer videoID;
+
+	@Column(table="Url")
     private String Url;
+
+	@Column(table="courseID")
 	private Integer courseID;
+
+	public Integer getVideoID() {
+		return this.videoID;
+	}
+
+	public void setVideoID(Integer videoID) {
+		this.videoID = videoID;
+	}
+
+	public String getUrl() {
+		return this.Url;
+	}
+
+	public void setUrl(String Url) {
+		this.Url = Url;
+	}
+
+	public Integer getCourseID() {
+		return this.courseID;
+	}
+
+	public void setCourseID(Integer courseID) {
+		this.courseID = courseID;
+	}
+
       
 
 }
