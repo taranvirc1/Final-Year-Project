d0d1792cdf83f92be0bd8f02f8897f88c9c3c7ef
KENURA <pererakenura@gmail.com>
2023-02-05 12:38:58 +0000

    update to student forum backend

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Messages.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Messages.java
@@ -1,5 +1,97 @@
 package CS2001.Group47.ELearning_Platform.model;
 
-public class Messages {
-    
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "Messages")
+@EntityListeners(AuditingEntityListener.class)
+public class Messages implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer messageID;
+	
+	@NotBlank
+	String message;
+	
+	@NotBlank
+	int mLikes;
+	
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAtDate;
+
+	public Messages() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Messages(String message, int mLikes, Date createdAtDate) {
+		super();
+		this.message = message;
+		this.mLikes = mLikes;
+		this.createdAtDate = createdAtDate;
+	}
+
+	public Integer getMessageID() {
+		return messageID;
+	}
+
+	public void setMessageID(Integer messageID) {
+		this.messageID = messageID;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getmLikes() {
+		return mLikes;
+	}
+
+	public void setmLikes(int mLikes) {
+		this.mLikes = mLikes;
+	}
+
+	public Date getCreatedAtDate() {
+		return createdAtDate;
+	}
+
+	public void setCreatedAtDate(Date createdAtDate) {
+		this.createdAtDate = createdAtDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Messages [messageID=" + messageID + ", message=" + message + ", mLikes=" + mLikes + ", createdAtDate="
+				+ createdAtDate + "]";
+	}	
+
 }
+
--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Subscriptions.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/model/Subscriptions.java
@@ -1,5 +1,108 @@
 package CS2001.Group47.ELearning_Platform.model;
 
-public class Subscriptions {
-    
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "Subscriptions")
+@EntityListeners(AuditingEntityListener.class)
+public class Subscriptions implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer subIid;
+	
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAtDate;
+	
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date updatedAt;
+
+//	@ManyToOne
+//	@JoinColumn(name = "Students")
+//	private Student student;
+//	
+//	@ManyToOne
+//	@JoinColumn(name = "Threads")
+//	private Threads thread;
+
+	public Subscriptions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Subscriptions(Date createdAtDate, Date updatedAt) {
+		super();
+		this.createdAtDate = createdAtDate;
+		this.updatedAt = updatedAt;
+	}
+
+	public Integer getSubIid() {
+		return subIid;
+	}
+
+
+
+	public void setSubIid(Integer subIid) {
+		this.subIid = subIid;
+	}
+
+
+
+	public Date getCreatedAtDate() {
+		return createdAtDate;
+	}
+
+
+
+	public void setCreatedAtDate(Date createdAtDate) {
+		this.createdAtDate = createdAtDate;
+	}
+
+
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Subscriptions [subIid=" + subIid + ", createdAtDate=" + createdAtDate + ", updatedAt=" + updatedAt
+				+ "]";
+	}
+	
+
 }
