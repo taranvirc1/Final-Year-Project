20aff850b0d4b262af6116c066850f3b523140d9
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-06 12:09:45 +0000

    Fix errors when running backend

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/repository/CoursesRepository.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/repository/CoursesRepository.java
@@ -3,8 +3,10 @@ package CS2001.Group47.ELearning_Platform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import CS2001.Group47.ELearning_Platform.model.Courses;
+
 @Repository
 
-public interface CoursesRepository extends JpaRepository{
+public interface CoursesRepository extends JpaRepository<Courses, Integer>{
     
 }
