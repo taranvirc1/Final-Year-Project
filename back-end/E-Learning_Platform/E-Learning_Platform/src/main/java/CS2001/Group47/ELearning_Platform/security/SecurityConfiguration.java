6104c2487425a54a00a90603a2f58e23da62179e
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-11 14:49:11 +0000

    Create JWTAuthFilter and SecurityConfig in backend

---
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/security/SecurityConfiguration.java
@@ -0,0 +1,78 @@
+package CS2001.Group47.ELearning_Platform.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+    
+    @Autowired
+    private StudentDetailsServiceImplementation studentDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(studentDetailsService)
+        .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/user")
+            .permitAll()
+            .and()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(new LoginFilter("/login", authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))),
+                UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JWTAuthenticationFilter(), 
+                UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+
+    /*
+	//If you want to configure an InMemory User for testing
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User
+				.withUsername("user")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	   public PasswordEncoder passwordEncoder() {
+	       return new BCryptPasswordEncoder();
+	   } 
+	   */
+
+}
