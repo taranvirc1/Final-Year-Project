69738d5b359cbbe3ba42450978cb7b371e034dee
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-11 14:29:10 +0000

    Create JWTService and LoginFilter for security in backend

---
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/security/LoginFilter.java
@@ -0,0 +1,41 @@
+package CS2001.Group47.ELearning_Platform.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public LoginFilter(String url, AuthenticationManager aManager) {
+        super(new AntPathRequestMatcher(url));
+        this.setAuthenticationManager(aManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        // TODO Auto-generated method stub
+        AccountCredentials credentials = new ObjectMapper().readValue(request.getInputStream(), AccountCredentials.class);
+
+        return this.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(credentials.getEmail(), credentials.getPassword(), Collections.emptyList()));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        JWTService.addToken(response, auth.getName());
+    }
+    
+}
