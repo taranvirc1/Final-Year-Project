1d52dc8087c2dd7c946966f437fb4cb14c0cf064
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-13 12:53:55 +0000

    Revert "Revert "Uncomment security code""
    This reverts commit 4fc5dd3a9bfe4f60ee52a0841798a068a7a07af5.

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/controller/StudentController.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/controller/StudentController.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,11 +51,10 @@ public class StudentController {
         }
 
         // Create password encoder object
-        // BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         //Else create a student with DTO
-        Student newStudent = new Student(newStudentDTO.getFirstName(), newStudentDTO.getLastName(), newStudentDTO.getDateOfBirth(), newStudentDTO.getCountry(), newStudentDTO.getPhone(), newStudentDTO.getEmail(), newStudentDTO.getPassword());
-        // Student newStudent = new Student(newStudentDTO.getFirstName(), newStudentDTO.getLastName(), newStudentDTO.getDateOfBirth(), newStudentDTO.getCountry(), newStudentDTO.getPhone(), newStudentDTO.getEmail(), encoder.encode(newStudentDTO.getPassword()));
+        Student newStudent = new Student(newStudentDTO.getFirstName(), newStudentDTO.getLastName(), newStudentDTO.getDateOfBirth(), newStudentDTO.getCountry(), newStudentDTO.getPhone(), newStudentDTO.getEmail(), encoder.encode(newStudentDTO.getPassword()));
         //Add student through StudentService
         studentService.addStudent(newStudent);
 
--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/security/JWTService.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/security/JWTService.java
@@ -1,53 +1,53 @@
-// package CS2001.Group47.ELearning_Platform.security;
+package CS2001.Group47.ELearning_Platform.security;
 
-// import java.util.Date;
+import java.util.Date;
 
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 
-// import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
-// import static java.util.Collections.emptyList;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import static java.util.Collections.emptyList;
 
-// public class JWTService {
+public class JWTService {
 
-//     static final long EXPIRATIONKEY = 864_000_00; //1 day in milliseconds
-//     static final String SIGNINKEY = "SecretKey";
-//     static final String PREFIX = "Bearer";
+    static final long EXPIRATIONKEY = 864_000_00; //1 day in milliseconds
+    static final String SIGNINKEY = "SecretKey";
+    static final String PREFIX = "Bearer";
 
-//     // Add token to Authorization header
-//     static public void addToken(HttpServletResponse response, String email) {
-//         String JwtToken = Jwts.builder().setSubject(email)
-//             .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONKEY))
-//             .signWith(SignatureAlgorithm.HS512, SIGNINKEY)
-//             .compact();
-//         System.out.println(JwtToken);
-//         response.addHeader("Authorization", PREFIX + " " + JwtToken);
-//         response.addHeader("Access-Control-Expose-Headers", "Authorization");
-//     }
+    // Add token to Authorization header
+    static public void addToken(HttpServletResponse response, String email) {
+        String JwtToken = Jwts.builder().setSubject(email)
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONKEY))
+            .signWith(SignatureAlgorithm.HS512, SIGNINKEY)
+            .compact();
+        System.out.println(JwtToken);
+        response.addHeader("Authorization", PREFIX + " " + JwtToken);
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
+    }
 
-//     // Get token from Authorization header
-//     static public Authentication getAuthentication(HttpServletRequest request) {
-//         String token = request.getHeader("Authorization");
-//         if(token != null) {
-//             try {
-//                 String student = Jwts.parser()
-//                     .setSigningKey(SIGNINKEY)
-//                     .parseClaimsJws(token.replace(PREFIX, ""))
-//                     .getBody()
-//                     .getSubject();
-//                 if(student != null) {
-//                     return new UsernamePasswordAuthenticationToken(student, null, emptyList());
-//                 } 
-//             } 
-//                 catch(Exception e) {
-//                     System.out.println(e.getMessage());
-//                 }
-//         }
-//         return null;
-//     }
+    // Get token from Authorization header
+    static public Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if(token != null) {
+            try {
+                String student = Jwts.parser()
+                    .setSigningKey(SIGNINKEY)
+                    .parseClaimsJws(token.replace(PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+                if(student != null) {
+                    return new UsernamePasswordAuthenticationToken(student, null, emptyList());
+                } 
+            } 
+                catch(Exception e) {
+                    System.out.println(e.getMessage());
+                }
+        }
+        return null;
+    }
     
-// }
+}
