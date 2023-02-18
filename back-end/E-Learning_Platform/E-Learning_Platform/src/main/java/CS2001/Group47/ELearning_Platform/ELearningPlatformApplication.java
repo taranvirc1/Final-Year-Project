33ba74eb3dc058374b45e0e1ecaa6e7703bfe64b
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-18 12:13:05 +0000

    Update ELearningPlatformApplication.java

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/ELearningPlatformApplication.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/ELearningPlatformApplication.java
@@ -3,7 +3,6 @@ package CS2001.Group47.ELearning_Platform;
 import java.util.Arrays;
 
 import org.springframework.context.ApplicationContext;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// import CS2001.Group47.ELearning_Platform.model.Student;
-// import CS2001.Group47.ELearning_Platform.model.Courses;
-// import CS2001.Group47.ELearning_Platform.model.Videos;
-// import CS2001.Group47.ELearning_Platform.repository.CoursesRepository;
-// import CS2001.Group47.ELearning_Platform.repository.StudentRepository;
-// import CS2001.Group47.ELearning_Platform.repository.VideosRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -39,32 +31,10 @@ public class ELearningPlatformApplication {
 			}
 		};
 	}
-	// @Autowired 
-        // private CoursesRepository coursesrepository;
-
-        // @Autowired 
-        // private VideosRepository videosrepository;
-
-		// @Autowired 
-        // private StudentRepository studentRepository;
+	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-			/*Courses Java = new Courses("Java");
-            Courses Python = new Courses("Mary");
-            coursesrepository.save(Java);
-			coursesrepository.save(Python);
-
-      
-            // Add car object with link to owners and save these to db.
-            Videos javaVideos = new Videos("Youtube", "Mustang", Java);
-            videosrepository.save(javaVideos);
-            Videos pythonVideos = new Videos("Nissan", "Leaf",  Python);
-            videosrepository.save(pythonVideos);/* */
-
-			// Student student1 = new Student("Johann", "Cardona", "2001-06-06", "Colombia", "07553266228", "johcar20@hotmail.com", "1234567890");
-			// studentRepository.save(student1);
-
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 						String[] beanNames = ctx.getBeanDefinitionNames();
 					Arrays.sort(beanNames);
