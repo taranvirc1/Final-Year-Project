ac9280c1411b8cb0722950ae52ff19f4e2af5cc3
2007048_Johann_Cardona <2007048@brunel.ac.uk>
2023-02-27 15:23:16 +0000

    Revert "added changes that were deleted when updating from main"
    This reverts commit 12bc5fd83a86ba5b4c28031222ff6b8371916e58.

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/repository/ThreadsRepository.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/repository/ThreadsRepository.java
@@ -1,20 +1,8 @@
 package CS2001.Group47.ELearning_Platform.repository;
 
-import CS2001.Group47.ELearning_Platform.model.Threads;
-
-import java.util.Date;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThreadsRepository extends JpaRepository<Threads, Integer> {
-
-	Threads findByThreadName(String threadName);
-
-	Threads findByfTags(String fTags);
+public interface ThreadsRepository {
     
-    Threads findByfDateCreated(Date fDateCreated); 
-
 }
--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/ThreadService.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/ThreadService.java
@@ -1,34 +1,8 @@
 package CS2001.Group47.ELearning_Platform.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import CS2001.Group47.ELearning_Platform.model.Threads;
-import CS2001.Group47.ELearning_Platform.repository.ThreadsRepository;
 
 @Service
 public class ThreadService {
     
-    @Autowired
-	ThreadsRepository threadsRepository;
-
-	public ThreadService() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public List<Threads> getThreads() {
-		return (List<Threads>) threadsRepository.findAll();
-	}
-
-	public void addThread(Threads newThread) {
-		threadsRepository.save(newThread);
-	}
-
-	public Optional<Threads> findByID(Integer id) {
-		return threadsRepository.findById(id);
-	}
 }
