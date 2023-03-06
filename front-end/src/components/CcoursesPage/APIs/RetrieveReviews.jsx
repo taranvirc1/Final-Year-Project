557f5f11a1e165bb2bec928e9266b0df9ec07f12
Murad-gif <2051290@brunel.ac.uk>
2023-03-06 14:20:12 +0000

    added accordion and background

--- front-end/src/components/CcoursesPage/APIs/RetrieveReviews.jsx
+++ front-end/src/components/CcoursesPage/APIs/RetrieveReviews.jsx
@@ -2,21 +2,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
-
-    
-
-export const getReviews = async ({ setTotalReviews,average,setReviews,jwt}) => {
-    await axios
-      .get("http://localhost:8080/getReviews", {
-        headers: { Authorization: `Bearer ${jwt}` },
-      })
-      .then((response) => {
-        setReviews(response.data);
-        setTotalReviews(response.data.length);
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("cant get reviews");
-      });
-  };
+export const getReviews = async ({
+  setTotalReviews,
+  average,
+  setReviews,
+  jwt,
+}) => {
+  await axios
+    .get("http://localhost:8080/getReviews", {
+      headers: { Authorization: `Bearer ${jwt}` },
+    })
+    .then((response) => {
+      setReviews(response.data);
+      setTotalReviews(response.data.length);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("cant get reviews");
+    });
+};
--- front-end/src/components/CcoursesPage/Accordion.jsx
+++ front-end/src/components/CcoursesPage/Accordion.jsx
@@ -1,96 +1,64 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 import ReactPlayer from "react-player";
 
-
 const Accordion = () => {
+  const jwt = sessionStorage.getItem("jwt");
 
-    const jwt = sessionStorage.getItem("jwt");
+  const [videoUrl, setVideoUrl] = useState("");
 
+  const [category, setCategory] = useState([]);
 
+  const course = 1;
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/categories`, {
+        headers: { Authorization: `Bearer ${jwt}` },
+      })
+      .then((resp) => {
+        console.log(resp.data);
 
-    const [videoUrl, setVideoUrl] = useState("");
+        setCategory(resp.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
+  console.log("cat" + JSON.stringify(category));
 
+  const retrieveUrl = (videoName, courseID) => {
+    const i = category.find(
+      (video) =>
+        video.videoName === videoName && video.course.courseID === courseID
+    );
+    const url = i.url;
+  };
 
-    const [category, setCategory] = useState([]);
+  const [showPlayer, setShowPlayer] = useState(false);
 
-    const  course = 1;
+  function playVideo(url) {
+    setVideoUrl(url);
+    setShowPlayer(true);
+  }
 
-
-  
-  
-    useEffect( () => {
-        axios.get(`http://localhost:8080/categories`,{  headers: {"Authorization" : `Bearer ${jwt}`} })
-          .then(resp =>  {
-            console.log(resp.data);
-       
-            setCategory(resp.data);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-       
-         }, []);
-       
-       
-    console.log("cat" +JSON.stringify(category))
-        
-       
-       
-       const retrieveUrl = (videoName,courseID) => {
-       
-       
-         const i =category.find((video)=>video.videoName===videoName && video.course.courseID === courseID);
-        const url= i.url;
-       
-       
-       
-       }
-       
-       const [showPlayer, setShowPlayer] = useState(false);
-
-
-
-
-
-       function playVideo(url) {
-        setVideoUrl(url);
-        setShowPlayer(true);
-      }
-
-
-    
   const [activeIndex, setActiveIndex] = useState(null);
   const [rotate, setRotate] = useState(null);
 
   const colors = ["#a0acea", "#fe9687", "#ffe587"];
- 
 
   const handleClick = (index) => {
     setActiveIndex(index === activeIndex ? null : index);
-setRotate(index ===activeIndex ? "rotate(0)" : "rotate(180deg)")
-
+    setRotate(index === activeIndex ? "rotate(0)" : "rotate(180deg)");
   };
 
-console.log(videoUrl)
-
-
-
-
-
-
-
-
+  console.log(videoUrl);
 
   return (
-
-<div>
-
-
-    {showPlayer && (
+    <div>
+      {showPlayer && (
         <div className="video-player-popup">
           <button className="closeIcon" onClick={() => setShowPlayer(false)}>
             <i class="fa-sharp fa-solid fa-xmark fa-2x" size={"10px"}></i>
@@ -105,39 +73,40 @@ console.log(videoUrl)
         </div>
       )}
 
-    <div className="sidenava" style={{ position: "relative", zIndex: 40 }}>
-      {category.map((item, index) => (
-        <>
-          <div
-            className="dropdown-btn"
-            key={index}
-            style={{ backgroundColor: colors[index] }}
-            onClick={() => handleClick(index)}
-          >
-            <span className="line-2"></span>
-            <span className="line-1">{item.categoryName}</span>
-            <i className="fa fa-caret-down"  ></i>
-
-          </div>
-          <div>
-            {activeIndex === index && (
-              <div className="dropdown-container">
-                {item.videos.map((link, i) => (
-                  <a href="#!" key={i} onClick={() => {
-
-
-                
-                    playVideo(link.url)
-            }}>
-                    {link.videoName}<i className="fa-regular fa-circle-play"></i>
-                  </a>
-                ))}
-              </div>
-            )}
-          </div>
-        </>
-      ))}
-    </div>
+      <div className="sidenava" style={{ position: "relative", zIndex: 2 }}>
+        {category.map((item, index) => (
+          <>
+            <div
+              className="dropdown-btn"
+              key={index}
+              style={{ backgroundColor: colors[index] }}
+              onClick={() => handleClick(index)}
+            >
+              <span className="line-2"></span>
+              <span className="line-1">{item.categoryName}</span>
+              <i className="fa fa-caret-down"></i>
+            </div>
+            <div>
+              {activeIndex === index && (
+                <div className="dropdown-container">
+                  {item.videos.map((link, i) => (
+                    <a
+                      href="#!"
+                      key={i}
+                      onClick={() => {
+                        playVideo(link.url);
+                      }}
+                    >
+                      {link.videoName}
+                      <i className="fa-regular fa-circle-play"></i>
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
