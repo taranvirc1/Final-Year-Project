73a097806fe34052e7ef69b8ab1fc8c92ba2fdc3
Murad-gif <2051290@brunel.ac.uk>
2023-03-15 15:42:18 +0000

    edited the sponsors section for the home page

--- front-end/src/components/CcoursesPage/CoursesVideos.jsx
+++ front-end/src/components/CcoursesPage/CoursesVideos.jsx
@@ -7,6 +7,15 @@ import { Link } from "react-router-dom";
 import { getReviews } from "./APIs/RetrieveReviews";
 import Background from "./background/Background";
 import Accordion from "./Accordion";
+
+import img1 from"../../images/footer image/brunel-logo-blue.png"
+import img2 from "../../images/footer image/codecademy-logo-vector.png"
+import img3 from "../../images/footer image/Logo_of_the_United_Nations.svg.png"
+import img4 from "../../images/footer image/ioc_logo_onwhite_aw.258x0-is-hidpi.png"
+
+
+
+
 function CoursesVideos() {
   const reDirect = useNavigate();
 
@@ -369,9 +378,9 @@ function CoursesVideos() {
       <Background />
 
       <div className="discription">
-        <h1 className="header">Learn C# Programming (In Ten Easy Steps) </h1>
+        <h1 className="header">Learn Java Programming (In Ten Easy Steps) </h1>
         <h2 className="sub-header">
-          The simplest way to learn C# programming.
+          The simplest way to learn Java programming.
         </h2>
       </div>
       <div className="objectVideos">
@@ -380,12 +389,12 @@ function CoursesVideos() {
           <li>Use the source code examples to learn step-by-step </li>
         </div>
         <div className="b">
-          <li>Master C# programming concepts from the ground up</li>
+          <li>Master Java programming concepts from the ground up</li>
           <div className="c">
             <li>
               {" "}
               Use the source code examples to learn step-by-step Understand the
-              special features of C#: object orientation, the .NET framework,
+              special features of Java: object orientation, the .NET framework,
               error-handling, serialization
             </li>
           </div>
@@ -420,23 +429,17 @@ function CoursesVideos() {
       <div className="Coursediscription">
         <h1 className="DescriptionHeader">Discription </h1>
         <div className="DescriptionText">
-          Learn C# Programming (in ten easy steps) [Version 2] is suitable for
-          beginner programmers or anyone with experience in another programming
-          language who needs to learn C# from the ground up. Step-by-step it
-          explains how to write C# code to develop Windows applications using
-          either the free Visual Studio Community Edition or a commercial
-          edition of Microsoft Visual Studio (it even explains how to write C#
-          programs using free tools for OS X). This is the completely revised
-          and updated second version of this course. C# is one of the most
-          widely used an important of all modern programming languages. If you
-          need to learn C# quickly and painlessly, this is the perfect course.
-          You will begin by learning the core features of programming variables,
-          constants, functions and data types. You will move on rapidly to learn
-          about Object Orientation and the more advanced features of C# and the
-          .NET framework such as file-handling, data-streaming, dealing with
-          exceptions (errors) and overriding methods. Even if you start out as a
-          complete beginner, by the end of this course you will have built a
-          really solid foundation of programming knowledge and skills.
+   
+The course consists of JAVA features, Java SE
+Concept of programming are made simple and easy.
+Every topic is explained with real-life examples.
+This course is designed to make you familiar with JAVA Programming in detail.
+By the end of the course you will understand Java extremely well and will be able to build your own Java applications.
+After completion of the course, you will be as productive as a software developer.
+The course is taken right from basics to all the features in JAVA.
+Basic topics like – Methods, Object-Orientation and Inheritance are explained.
+Features like – Multithreading, AWT, Swing, Collection Framework and Networking are also covered in a detailed manner.
+
         </div>
       </div>
 
@@ -572,7 +575,7 @@ function CoursesVideos() {
           <div className="sponsor-feature">
             <img
               alt=""
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Logo_of_the_United_Nations.svg/1200px-Logo_of_the_United_Nations.svg.png"
+              src={img1}
               style={{ width: "200px" }}
             />
           </div>
@@ -580,7 +583,7 @@ function CoursesVideos() {
           <div className="sponsor-feature">
             <img
               alt=""
-              src="https://www.dcs.bbk.ac.uk/site/assets/files/4102/ioc_logo_onwhite_aw.258x0-is-hidpi.png"
+              src={img2}
               style={{ width: "155px" }}
             />
           </div>
@@ -588,7 +591,7 @@ function CoursesVideos() {
           <div className="sponsor-feature">
             <img
               alt=""
-              src="https://cdn.freebiesupply.com/logos/large/2x/codecademy-logo-svg-vector.svg"
+              src={img3}
               style={{ width: "155px" }}
             />
           </div>
@@ -596,7 +599,7 @@ function CoursesVideos() {
           <div className="sponsor-feature">
             <img
               alt=""
-              src="https://cdn.freebiesupply.com/logos/large/2x/codecademy-logo-svg-vector.svg"
+              src={img4}
               style={{ width: "155px" }}
             />
           </div>
