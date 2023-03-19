07021420a79d416ac99cc5eb9b87f83eb6cfe5a5
2118476 <117785648+2118476@users.noreply.github.com>
2023-03-19 12:59:29 +0000

    quiz

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/AnswerService.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/AnswerService.java
@@ -3,7 +3,11 @@ package CS2001.Group47.ELearning_Platform.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import CS2001.Group47.ELearning_Platform.model.Answer;
+import CS2001.Group47.ELearning_Platform.repository.AnswerRepository;
 
 public interface AnswerService {
   
@@ -18,4 +22,41 @@ public interface AnswerService {
   List<Answer> getAnswersByStudentId(Long studentId);
   
   List<Answer> getAnswersByQuestionId(Long questionId);
+}
+
+@Service
+class AnswerServiceImpl implements AnswerService {
+  
+  @Autowired
+  private AnswerRepository answerRepository;
+
+  @Override
+  public List<Answer> getAllAnswers() {
+    return answerRepository.findAll();
+  }
+
+  @Override
+  public Optional<Answer> getAnswerById(Long id) {
+    return answerRepository.findById(id);
+  }
+
+  @Override
+  public Answer saveAnswer(Answer answer) {
+    return answerRepository.save(answer);
+  }
+
+  @Override
+  public void deleteAnswerById(Long id) {
+    answerRepository.deleteById(id);
+  }
+  
+  @Override
+  public List<Answer> getAnswersByStudentId(Long studentId) {
+    return answerRepository.findByStudentId(studentId);
+  }
+  
+  @Override
+  public List<Answer> getAnswersByQuestionId(Long questionId) {
+    return answerRepository.findByQuestionId(questionId);
+  }
 }
--- front-end/src/components/Quizzes/Data/Categories.js
+++ front-end/src/components/Quizzes/Data/Categories.js
@@ -2,7 +2,7 @@ const Categories = [
   { category: "Python", value: "Python" },
   { category: "JAVA", value: "Java" },
   { category: "FrontEnd", value: "FrontEnd" },
-  // { category: "C++", value: "C++" },
+   { category: "C++", value: "C++" },
 ];
 
 export default Categories;
