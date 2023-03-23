9044808a75329da7778cde277a431a3a785d1edb
KENURA <pererakenura@gmail.com>
2023-03-23 12:30:51 +0000

    update

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/SubscriptionService.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/SubscriptionService.java
@@ -22,7 +22,7 @@ public class SubscriptionService {
 	}
 
 
-    public Iterable<Subscriptions> findSub(String subEmail, Integer threadId) {
+    public Subscriptions findSub(String subEmail, Integer threadId) {
 		return subscriptionRepository.findBySubEmailAndThreads_ThreadId(subEmail, threadId);
     }
     
