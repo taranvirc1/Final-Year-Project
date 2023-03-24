bd5962773df20ce3b6a27c9d579c49928a982369
2118476 <117785648+2118476@users.noreply.github.com>
2023-03-24 21:59:19 +0000

    quiz

--- back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/ResultService.java
+++ back-end/E-Learning_Platform/E-Learning_Platform/src/main/java/CS2001/Group47/ELearning_Platform/service/ResultService.java
@@ -1,50 +1,32 @@
 package CS2001.Group47.ELearning_Platform.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import CS2001.Group47.ELearning_Platform.model.Result;
+import CS2001.Group47.ELearning_Platform.model.Student;
+import CS2001.Group47.ELearning_Platform.repository.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import CS2001.Group47.ELearning_Platform.model.Result;
-import CS2001.Group47.ELearning_Platform.repository.ResultRepository;
-
-public interface ResultService {
-    Result saveResult(Result result);
-    List<Result> getAllResults();
-    Optional<Result> getResultById(Long id);
-    List<Result> getResultsByStudentId(Long studentId);
-    void deleteResultById(Long resultId);
-}
+import java.util.List;
 
 @Service
-class ResultServiceImpl implements ResultService {
-  
-  @Autowired
-  private ResultRepository resultRepository;
+public class ResultService {
 
-  @Override
-  public List<Result> getAllResults() {
-    return resultRepository.findAll();
-  }
+    @Autowired
+    private ResultRepository resultRepository;
 
-  @Override
-  public Optional<Result> getResultById(Long id) {
-    return resultRepository.findById(id);
-  }
+    public Result saveResult(Result result) {
+        return resultRepository.save(result);
+    }
 
-  @Override
-  public Result saveResult(Result result) {
-    return resultRepository.save(result);
-  }
+    public List<Result> getResults() {
+        return resultRepository.findAll();
+    }
 
-  @Override
-  public List<Result> getResultsByStudentId(Long studentId) {
-    return resultRepository.findByStudent(studentId);
-  }
+    public Result getResultById(Long id) {
+        return resultRepository.findById(id).orElse(null);
+    }
 
-  @Override
-  public void deleteResultById(Long resultId) {
-    throw new UnsupportedOperationException("Unimplemented method 'deleteResultById'");
-  }
+    public List<Result> getResultsByStudent(Student student) {
+        return resultRepository.findByStudent(student);
+    }
 }
